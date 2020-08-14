7d9bcdb55149e9a4d01f9e3b053bd874c2d714f6
Abhishek <abhishek.sengupta@cbnits.com>
2020-08-14 23:41:15 +0530

    Init Commit

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,6 +5,7 @@ object Main extends App {
   println(Tokenizer.splitSentenceToTokens("hellohowareyoubro"))
   println(Tokenizer.splitSentenceToTokens("howisitgoing?"))
   println(Tokenizer.splitSentenceToTokens("whatisyourname?"))
+  println(Tokenizer.splitSentenceToTokens("isislandagoodplacetolive??"))
   println(Tokenizer.splitSentenceToTokens("enemyatthegates"))
 
 }
